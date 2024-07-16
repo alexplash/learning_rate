f4ff8de1c3ab0cadb2c8e047f3ace8b2b0d74d02
Alexandr Plashchinsky <aplashch@gmail.com>
2024-07-15 21:58:51 -0700

    commit

---
+++ backend/data/genData_music.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+
+num_rows = 1000
+
+df = pd.DataFrame({
+    'Spotify Streams': np.random.randint(10000, 10000000, size = num_rows),
+    'Spotify Playlist Count': np.random.randint(1, 1000, size = num_rows),
+    'Youtube Likes': np.random.randint(1000, 500000, size = num_rows),
+    'TikTok Likes': np.random.randint(1000, 500000, size = num_rows),
+    'Apple Music Playlist Count': np.random.randint(1, 1000, size = num_rows),
+    'Amazon Playlist Count': np.random.randint(1, 1000, size = num_rows),
+    'Explicit Track': np.random.choice([0, 1], size = num_rows)
+})
+
+df.to_csv('explicit_music.csv', index = False)
